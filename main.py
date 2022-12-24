f096a147db2a1a1243901fc4963df3fa4055a765
Natalia Gryaznova <n.gryaznova@yadro.com>
2022-12-24 18:33:15 +0300

    Fix review comments

--- main.py
+++ main.py
@@ -95,9 +95,10 @@ def book_gen(start_pk: int = 1) -> Generator:
     :param start_pk: счетчик книг, автоинкремент, по умолчанию 1.
     :return: генератор книг.
     """
-    dict_ = dict()
+    #dict_ = dict()
     pk = start_pk
     for _ in range(100):
+        dict_ = dict()
         dict_["model"] = MODEL
         dict_["pk"] = pk
         dict_["fields"] = dict()
